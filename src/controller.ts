683039e6585a2adafc4c27981a359fba884bd747
Marc Brooks <IDisposable@gmail.com>
2022-11-16 22:16:29 -0600

    Simplified the response from publish
    It was nesting array and emitting odd json.

--- src/controller.ts
+++ src/controller.ts
@@ -102,15 +102,11 @@ export const diff = async (): Promise<Response> => {
   return new Response(
     JSON.stringify({
       messages: [
-        [
           `There are ${addedRules.length} rules to add (in spreadsheet but not published).`,
-        ],
-        [
           `There are ${removedRules.length} rules to remove (published but not in spreadsheet).`,
         ],
-        { addedRules },
-        { removedRules },
-      ],
+        addedRules: addedRules,
+        removedRules: removedRules,
     }),
     {
       headers: { 'content-type': 'application/json' },
